af48813c6b7391565145fc23994a8e5f2df3094d
Fernando Freire <fernando.ricardo.freire@gmail.com>
2025-09-21 16:36:42 -0300

    :sparkles: feat: add biome lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,25 @@
-import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google'
+import './globals.css'
 
 const nunito = Nunito({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+	variable: '--font-geist-sans',
+	subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-  title: "Nexus Commerce",
-  description: "A nossa plataforma de e-commerce",
-};
+	title: 'Nexus Commerce',
+	description: 'A nossa plataforma de e-commerce',
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${nunito.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${nunito.variable} antialiased`}>{children}</body>
+		</html>
+	)
 }
