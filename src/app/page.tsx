277237e03064d4e41bb4afa12b12fd8a476f819a
Fernando Freire <fernando.ricardo.freire@gmail.com>
2025-09-21 19:43:37 -0300

    :sparkles: feat: add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
 	return (
 		<main>
 			<h1>Nexus Commerce</h1>
+			<Button>Get Started</Button>
 		</main>
 	)
 }
